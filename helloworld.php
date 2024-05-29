90d556c8fbb49821c7e4507604d56242b4860d01
yellow3to7 <yellow3to7>
2024-05-29 09:44:04 +0700

    first commit

---
+++ helloworld.php
@@ -0,0 +1,8 @@
+<?php
+namespace iqlima\konstruksi;
+
+class helloworld{
+    public function sayhello ($name) {
+        return"hello $name welcome to php and composer to create your own library";
+    }
+}
